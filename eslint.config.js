be4c6b80bb6c6faddd9394adc5f09170492dbeec
Web-Developer-DB <webdev.digidb@gmail.com>
2025-10-28 07:45:15 +0100

    Fehlerbehebung und ESLint-Konfiguration

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "no-unused-vars": "off",
+      "no-undef": "off",
+      "no-prototype-builtins": "off",
+      "no-misleading-character-class": "off",
+      "no-useless-escape": "off",
+      "no-cond-assign": "off",
+      "no-empty": "off",
+      "valid-typeof": "off",
+      "no-fallthrough": "off",
+      "no-control-regex": "off",
+      "getter-return": "off",
+      "no-constant-condition": "off",
+      "no-func-assign": "off",
+      "react/display-name": "off",
+    },
+  },
+]);
